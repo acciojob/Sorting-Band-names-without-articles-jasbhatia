18485ed4aadf9f727cabd04a31e0e755b1e38400
JASPREET KAUR <94101161+jasbhatia@users.noreply.github.com>
2023-03-14 17:44:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
 //your code here
 
+function strip(Spot)
+{
+	return touristName.replace(/^(a| the|an)/i,'').trim();
+	
+}
+const sorted=touristSpot.sort((a,b)=>{
+	return strip(a)>strip(b)?1:-1;
+});
