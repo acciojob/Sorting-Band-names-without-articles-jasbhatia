6ced8089257e7e993e344e8e31a4cc9fb74db389
JASPREET KAUR <94101161+jasbhatia@users.noreply.github.com>
2023-03-15 16:55:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function strip(Spot)
 	return touristName.replace(/^(a| the|an)/i,'').trim();
 	
 }
-const sorted=touristSpot.sort((a,b)=>{
+const sorted=Spot.sort((a,b)=>{
 	return strip(a)>strip(b)?1:-1;
 });
 document.querySelector('#bands').innerHTML= sorted.map(band=>`<li>{band}<li>`);
