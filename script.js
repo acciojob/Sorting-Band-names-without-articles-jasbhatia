4e2c96a92035025f6de5c894a2c057b6201ebcca
JASPREET KAUR <94101161+jasbhatia@users.noreply.github.com>
2023-03-16 14:07:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
 //your code here
+const bands = [
+    "The Plot in You",
+    "The Devil Wears Prada",
+    "Pierce the Veil",
+    "Norma Jean",
+    "The Bled",
+    "Say Anything",
+    "The Midway State",
+    "We Came as Romans",
+    "Counterparts",
+    "Oh, Sleeper",
+    "A Skylit Drive",
+    "Anywhere But Here",
+    "An Old Dog"
+];
 
-function strip(Spot)
-{
-	return Spot.replace(/^(a| the|an)/i,'').trim();
-	
+function strip (word) {
+    let regex = new RegExp('^(a |the |an )', 'i')
+    return word.replace(regex, '').trim()
 }
-const sorted=Spot.sort((a,b)=>{
-	return strip(a)>strip(b)?1:-1;
-});
-document.querySelector('#bands').innerHTML= sorted.map(band=>`<li>{band}<li>`);
-console.log(sorted);
+const sortedBands = bands.sort((a, b) => (strip(a) > strip(b)) ? 1 : -1)
+document.querySelector('#bands').innerHTML = 
+    sortedBands.map(item => `<li>${item}</li>`).join('');
